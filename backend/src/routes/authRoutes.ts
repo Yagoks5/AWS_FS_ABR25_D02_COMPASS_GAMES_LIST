f6d1459b81a938c96ed1c214611427ca49badb87
Yago <yago.ks5@gmail.com>
2025-05-28 10:29:24 -0300

    fix: fix routes and cors

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,15 +1,19 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/authController';
-import { Request, Response, NextFunction } from 'express';
+import { Router } from "express";
+import { register, login, getUsers } from "../controllers/authController";
+import { Request, Response, NextFunction } from "express";
 
 const router = Router();
 
-router.post('/register', (req: Request, res: Response, next: NextFunction) => {
+router.post("/register", (req: Request, res: Response, next: NextFunction) => {
   register(req, res).catch(next);
 });
 
-router.post('/login', (req: Request, res: Response, next: NextFunction) => {
+router.post("/login", (req: Request, res: Response, next: NextFunction) => {
   login(req, res).catch(next);
+});
+
+router.get("/users", (req: Request, res: Response, next: NextFunction) => {
+  getUsers(req, res).catch(next);
 });
 
 export default router;
