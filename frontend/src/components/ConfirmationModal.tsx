e7982342f8ded4ec211f80f22c6d9b0be3f0a387
VictorJunior-creator <victorjuniorporto@gmail.com>
2025-06-04 10:59:15 -0300

    Feat: update ConfirmationModal component

--- frontend/src/components/ConfirmationModal.tsx
+++ frontend/src/components/ConfirmationModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ConfirmationModal.css';
+import { TbAlertTriangleFilled } from "react-icons/tb";
 
 interface ConfirmationModalProps {
   isOpen: boolean;
@@ -20,11 +21,7 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
     <div className="modal-overlay">
       <div className="confirmation-modal-content">
         <div className="confirmation-icon">
-          <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M32 5.33334L59.3333 53.3333H4.66667L32 5.33334Z" fill="#FF4B55" />
-            <path d="M32 40V24" stroke="white" strokeWidth="6" strokeLinecap="round" />
-            <circle cx="32" cy="48" r="3" fill="white" />
-          </svg>
+          <TbAlertTriangleFilled />
         </div>
         <h2 className="confirmation-title">Are you sure?</h2>
         <p className="confirmation-message">{message}</p>
